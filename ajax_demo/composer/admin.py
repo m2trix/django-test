8ad1ceb2db0e082430ccd4b01647119b78eb92e7
m2trix <m2trix@qq.com>
2018-09-07 01:30:54 +0800

    add data model for demo

--- ajax_demo/composer/admin.py
+++ ajax_demo/composer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UrlHead, UrlBody
 
-# Register your models here.
+@admin.register(UrlHead)
+class UrlHeadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url_env', 'url_domain', 'url_port', 'is_deleted')
+
+@admin.register(UrlBody)
+class UrlBodyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url_name', 'url_path', 'url_params', 'is_deleted')
--- ajax_demo/composer/models.py
+++ ajax_demo/composer/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class UrlHead(models.Model):
+    url_env = models.CharField(max_length=10)
+    url_domain = models.CharField(max_length=50)
+    url_port = models.CharField(max_length=10)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.url_env
+
+class UrlBody(models.Model):
+    url_name = models.TextField(default='default_string')
+    url_path = models.TextField()
+    url_params = models.TextField()
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.url_path
