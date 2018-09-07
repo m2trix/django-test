002e3fd3ddc45a265f82eb6c31b365a4f13e6dc6
pengfei.wei <pengfei.wei@wheatek.com>
2018-09-07 18:11:32 +0800

    modify for main logic

--- ajax_demo/composer/views.py
+++ ajax_demo/composer/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
+from .models import UrlBody
 import requests
 import json
 
 # Create your views here.
 def ajax_view(request):
-    context = {'code':0}
+    context = {}
+    print(request)
+    print(request.GET.get('body_pk', '1'))
 
     if request.is_ajax():
-        r = requests.get('http://httpbin.org/ip')
+        body_pk = request.GET.get('body_pk', '1')
+        url_body = get_object_or_404(UrlBody, pk=body_pk)
+
+        # r = requests.get('http://httpbin.org/ip')
+        full_url = ""
+        if "\'\'" != url_body.url_params:
+            full_url = 'http://httpbin.org' + url_body.url_path + "?" + url_body.url_params
+        else:
+            full_url = 'http://httpbin.org' + url_body.url_path
+        print(full_url)
+
+        r = requests.get('full_url: ' + full_url)
         context['code'] = r.status_code
         return JsonResponse(context)
 
-    return render_to_response('base.html', context)
+    context['code'] = 0
+    context['api_list'] = UrlBody.objects.all()
+    return render(request, 'base.html', context)
